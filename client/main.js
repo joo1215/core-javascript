76d2019ac626c6bd693baf104ee83428a858be0d
joo1215 <yjjj1215@gmail.com>
2023-01-16 17:24:44 +0900

    [추가] 메모이제이션 패턴

--- client/main.js
+++ client/main.js
@@ -10,6 +10,7 @@ import {
   insertLast,
   visibleElement,
   invisibleElement,
+  memo,
   attr
  } from "./lib/index.js";
 
@@ -32,6 +33,13 @@ import {
 // 2. HTML 템플릿 만들기
 // 3. 템플릿 뿌리기 
 
+// [ 초기화 시키기 ]
+// 1. clearContent 로 정보 지우기
+// 2. total, count 초기화 
+// 3. 스크롤 밑으로 보내기 
+// 4. 메모이제이션 패턴 
+
+
 
 
 // 배열의 구조 분해 할당 
@@ -40,15 +48,29 @@ const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > bu
 const recordListWrapper = getNode('.recordListWrapper')
 
 
+
+memo('@tbody',()=>getNode('.recordListWrapper tbody'));
+
+
+
+
 // 특정 대상의 속성값을 가져오거나 / 설정할 수 있는 함수 
 
 
+
+
+/* -------------------------------------------------------------------------- */
+/* render                                                                     */
+/* -------------------------------------------------------------------------- */
+
 let count = 0;
 let total = 0;
+// redux
+// mobx
 
 function renderRecordListItem(){
   
-  let diceValue = Number(attr('#cube','data-dice'));
+  let diceValue = Number(attr(memo('cube'),'data-dice'));
 
   let template = /* html */ `
     <tr>
@@ -58,20 +80,9 @@ function renderRecordListItem(){
     </tr>
   `
   
-  insertLast('.recordListWrapper tbody',template)
+  insertLast(memo('@tbody'),template)
   recordListWrapper.scrollTop = recordListWrapper.scrollHeight
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -118,8 +129,10 @@ const handleRecord =()=>{
 
 const handleReset = () => {
 
+  count = 0;
+  total = 0;
   invisibleElement(recordListWrapper);
-  clearContents('.recordListWrapper tbody')
+  clearContents(memo('@tbody'))
 }
 
 
